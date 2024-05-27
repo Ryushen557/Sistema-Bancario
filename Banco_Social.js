6fb2862dc79189adc2befc5c96333e8b39acab48
White-Snake <140353943+Ryushen557@users.noreply.github.com>
2024-05-27 12:42:13 -0400

    Base del proyecto
    -Subida de archivos base para el funcionamiento del proyecto del sistema bancario

---
+++ Banco_Social.js
@@ -0,0 +1,31 @@
+const sistemaBancario = {
+    PrestamoMensual : [
+        {
+            id:1,
+            Mes: "Mayo",
+            CantidadP: 10,
+            porcentajeP: 16,
+            montoP: 500,
+            interesesGenerados: 10
+        },
+        {
+            id:2,
+            Mes: "Junio",
+            CantidadP: 10,
+            porcentajeP: 16,
+            montoP: 500,
+            interesesGenerados: 10
+        }
+    ],
+    AhorroMensual: [
+        {
+
+        }
+    ],
+    Cooperativa:[
+        {
+            
+        }
+    ]
+    
+}
