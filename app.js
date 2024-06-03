70a60811b5e522bd346b9cb93abf286097de958d
White-Snake <140353943+Ryushen557@users.noreply.github.com>
2024-06-03 13:24:06 -0400

    Add files via upload
    Actualizacion del archivo app.js para ejecutar los archivos de los controladores, rutas y vistas y demas funciones

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
+var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usuarioRouter = require('./routes/usuarios');
+const cuentaRouter = require('./routes/cuentas');
+var cooperativaRouter = require('./routes/cooperativas');
 
 var app = express();
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), ()=>
-console.log('el servidor esta escuchando en el puerto', app.get('port')))
+app.listen(app.get('port'), () =>
+  console.log(`El servidor está corriendo en el puerto ${app.get('port')}`)
+);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -25,21 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/usuarios', usuarioRouter);
+app.use('/cuentas', cuentaRouter);
+app.use('/cooperativas', cooperativaRouter);
 
 module.exports = app;
