02e7f2a977e944d7a492a1a4ded8b9fa59f05de3
White-Snake <140353943+Ryushen557@users.noreply.github.com>
2024-06-03 13:20:30 -0400

    Add files via upload
    Nueva base de datos del Banco

--- Banco_Social.js
+++ Banco_Social.js
@@ -1,31 +1,9 @@
-const sistemaBancario = {
-    PrestamoMensual : [
-        {
-            id:1,
-            Mes: "Mayo",
-            CantidadP: 10,
-            porcentajeP: 16,
-            montoP: 500,
-            interesesGenerados: 10
-        },
-        {
-            id:2,
-            Mes: "Junio",
-            CantidadP: 10,
-            porcentajeP: 16,
-            montoP: 500,
-            interesesGenerados: 10
-        }
-    ],
-    AhorroMensual: [
-        {
+let usuarios = [];
+let cuentas = [];
+let cooperativas = [];
 
-        }
-    ],
-    Cooperativa:[
-        {
-            
-        }
-    ]
-    
-}
+module.exports = {
+    usuarios,
+    cuentas,
+    cooperativas
+};
